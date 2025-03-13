f09128c4ee7ceee2d6bbfc60c5dc7abcbc5356d2
unknowntalsiman <tuo98558@temple.edu>
2025-03-13 11:48:50 -0400

    Implemented onItemSelectedListener

--- app/src/main/java/com/example/fragmentactivity/ColorFragment.kt
+++ app/src/main/java/com/example/fragmentactivity/ColorFragment.kt
@@ -1,10 +1,13 @@
 package com.example.fragmentactivity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
@@ -21,6 +24,19 @@ class ColorFragment : Fragment() {
             findViewById<Spinner>(R.id.spinner).apply{
                 adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item,
                     arrayOf("Blue", "Red", "White", "Black", "Magenta"))
+
+                onItemSelectedListener = object: OnItemSelectedListener{
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        p0?.run {
+                            view?.setBackgroundColor(Color.parseColor(getItemAtPosition(p2).toString()))
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                    }
+
+
+                }
 
             }
         }
